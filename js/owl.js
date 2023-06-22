2ef562f48a913ceafb52ce00c0ca593a0d132bf5
Mark <135705108+twentyonekyoto@users.noreply.github.com>
2023-06-22 17:07:07 +0300

    Create owl.js

---
+++ js/owl.js
@@ -0,0 +1,17 @@
+$('.feedback .owl-carousel').owlCarousel({
+    loop:true,
+    margin:20,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    },
+    dots:true
+})
